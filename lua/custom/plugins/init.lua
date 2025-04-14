977110cc8f731619704fcc144d870837079230d1
DiegoAndresGlez <diegoglez077@gmail.com>
2025-04-14 14:30:18 -0700

    Update for typescript custom init and kickstart init

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -6,45 +6,13 @@ return {
   {
     'pmizio/typescript-tools.nvim',
     dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
-    opts = {},
-  },
-
-  -- -- TypeScript LSP Configuration
-  {
-    'neovim/nvim-lspconfig',
-    config = function()
-      require('lspconfig').ts_ls.setup {
-        settings = {
-          javascript = {
-            format = {
-              tabSize = 2,
-              indentSize = 2,
-            },
-          },
-          typescript = {
-            format = {
-              tabSize = 2,
-              indentSize = 2,
-            },
-          },
-        },
-        on_attach = function(client, bufnr)
-          -- Disable formatting on save
-          client.server_capabilities.documentFormattingProvider = false
-        end,
-      }
-    end,
-  },
-
-  -- Filetype-specific settings for TypeScript files
-  {
-    'neovim/nvim-lspconfig',
-    config = function()
-      -- Apply settings specifically for TypeScript files
-      vim.api.nvim_create_autocmd('FileType', {
-        pattern = 'typescript,typescriptreact',
-        command = 'setlocal tabstop=2 shiftwidth=2 softtabstop=2 expandtab',
-      })
-    end,
+    opts = {
+      autoformat = false,
+      on_attach = function(client)
+        -- Disable tsserver formatting
+        client.server_capabilities.documentFormattingProvider = false
+        client.server_capabilities.documentRangeFormattingProvider = false
+      end,
+    },
   },
 }
